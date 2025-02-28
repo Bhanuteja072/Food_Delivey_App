575ef9d082595bfb69975b7c68a20611dddcfcbb
Bhanuteja072 <bhanutejapusarla@gmail.com>
2025-03-01 00:05:32 +0530

    Delete admin/backend directory

--- admin/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-
-
- export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://bhanutejapusarla:142536@cluster0.qgyqe.mongodb.net/food-del').then(()=>{
-        console.log("DB connected");
-    })
-}
