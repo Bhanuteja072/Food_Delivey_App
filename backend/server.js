5a4d4426661464398bfa939cac33d1c0c7f0398b
Bhanu <bhanutejapusarla@gmail.com>
2025-03-08 12:31:36 +0530

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 
 import 'dotenv/config'
+import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 
 //app config
@@ -26,6 +28,9 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use("/images",express.static("uploads"))
 app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+
+app.use("/api/order",orderRouter)
 
 
 
