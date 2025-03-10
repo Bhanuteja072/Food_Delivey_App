92e126aae37326feec6e3e25ebcf309b88b209fc
Bhanu <bhanutejapusarla@gmail.com>
2025-03-10 20:34:59 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 
  export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://Your credentials cluster0.qgyqe.mongodb.net/food-del').then(()=>{
-        console.log("DB connected");
+      await mongoose.connect('mongodb+srv://bhanutejapusarla:142536@cluster0.qgyqe.mongodb.net/food-del').then(()=>{
+       console.log("DB connected");
     })
 }
